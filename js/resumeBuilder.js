06037a2c6c9567c60cfe3f3064d9fcf3ec961fe3
Shadeira <shadeiranes@gmail.com>
2017-10-05 14:59:32 -0400

    Add files via upload

---
+++ js/resumeBuilder.js
@@ -0,0 +1,189 @@
+var DATA = '%data%';
+
+var bio = {
+  "name": "Shadeira Nesmith",
+    "role": "Front-end Web Developer",
+    "contacts": {
+        "mobile": "201-705-9614",
+        "email": "snesmith.code@gmail.com",
+        "github": "shadeira",
+        "location": "New York, NY"
+  },
+  "biopic": "images/resume_shadeira.jpg",
+    "skills": ["HTML", "CSS", "Javascript", "jQuery", "Shopfiy", "Sublime Text", "Adobe XD"],
+    "welcomeMessage": "Welcome to my Resume",
+
+  display: function() {
+    var formattedName = HTMLheaderName.replace(DATA, bio.name);
+    var formattedRole = HTMLheaderRole.replace(DATA, bio.role);
+
+    var formattedMobile = HTMLmobile.replace(DATA, bio.contacts.mobile);
+    var formattedEmail = HTMLemail.replace(DATA, bio.contacts.email);
+    var formattedGithub = HTMLgithub.replace(DATA, bio.contacts.github);
+    var formattedTwitter = HTMLtwitter.replace(DATA, bio.contacts.twitter);
+    var formattedLocation = HTMLlocation.replace(DATA, bio.contacts.location);
+
+    var formattedBioPic = HTMLbioPic.replace(DATA, bio.biopic);
+    var formattedWelcomeMessage = HTMLwelcomeMsg.replace(DATA, bio.welcomeMessage);
+
+    $('#header').prepend(formattedName + formattedRole);
+    $('#topContacts, #footerContacts').append(formattedMobile + formattedEmail +
+      formattedGithub + formattedTwitter + formattedLocation);
+    $('#header').append(formattedBioPic);
+    $('#header').append(formattedWelcomeMessage);
+
+    if (bio.skills.length > 0) {
+      $('#header').append(HTMLskillsStart);
+
+      for (var i = 0, len = bio.skills.length; i < len; i++) {
+        var formattedSkills = HTMLskills.replace(DATA, bio.skills[i]);
+
+        $('#skills').append(formattedSkills);
+      }
+    }
+  }
+};
+
+var work = {
+  jobs: [ {
+      employer: 'Caldwell University',
+      title: 'Media Relations Assistant',
+      location: 'Caldwell, NJ',
+      dates: 'September 2016- May 2017',
+      description: 'Edited Poadcast for school website content.'
+    
+
+    },
+    {
+      employer: 'Kerwin Communcations',
+      title: 'Account Services Assistant',
+      location: 'Caldwell , NJ',
+      dates: 'January 2016- July 2016',
+      description: 'Managed high end luxury jewelry company’s retailers’ budgets.'
+    }
+  ],
+  display: function() {
+    if (work.jobs.length > 0) {
+      for (var i = 0, len = work.jobs.length; i < len; i++) {
+        var formattedEmployer = HTMLworkEmployer.replace(DATA, work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace(DATA, work.jobs[i].title);
+        var formattedDates = HTMLworkDates.replace(DATA, work.jobs[i].dates);
+        var formattedLocation = HTMLworkLocation.replace(DATA, work.jobs[i].location);
+        var formattedDescription = HTMLworkDescription.replace(DATA, work.jobs[i].description);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+        $('#workExperience').append(HTMLworkStart);
+        $('.work-entry:last').append(formattedEmployerTitle);
+        $('.work-entry:last').append(formattedDates);
+        $('.work-entry:last').append(formattedLocation);
+        $('.work-entry:last').append(formattedDescription);
+      }
+    }
+  }
+};
+
+var projects = {
+  projects: [ {
+      'title': 'Udacity Portfolio',
+      'dates': '2017',
+      'description': 'Portfolio with examples of my pervious work from Udacity and personal site.',
+      'images': ['images/port.jpg']
+    },
+    {
+      'title': 'Protoype',
+      'dates': 'September 2017',
+      'description': 'Prototype I created from scratch using Adobe XD.',
+      'images': ['images/prototype.jpg']
+    },
+    {
+      'title': 'Bowling Form',
+      'dates': '2015',
+      'description': 'Food Menu Order form I created for a class project using Javascript.',             
+      'images': ['images/bowling.jpg'],
+      
+    }
+  ],
+  display: function() {
+    if (projects.projects.length > 0) {
+      for (var i = 0, iLen = projects.projects.length; i < iLen; i++) {
+        var formattedTitle = HTMLprojectTitle.replace(DATA, projects.projects[i].title);
+        var formattedDates = HTMLprojectDates.replace(DATA, projects.projects[i].dates);
+        var formattedDescription = HTMLprojectDescription.replace(DATA, projects.projects[i].description);
+
+        $('#projects').append(HTMLprojectStart);
+        $('.project-entry:last').append(formattedTitle);
+        $('.project-entry:last').append(formattedDates);
+        $('.project-entry:last').append(formattedDescription);
+
+        for (var j = 0, jLen = projects.projects[i].images.length; j < jLen; j++) {
+          var formattedImage = HTMLprojectImage.replace(DATA, projects.projects[i].images[j]);
+
+          $('.project-entry:last').append(formattedImage);
+        }
+      }
+    }
+  }
+};
+
+var education = {
+  schools: [ {
+      'name': 'Caldwell University',
+      'location': 'Caldwell, NJ',
+      'degree': 'BA',
+      'majors': ['Comunications and Media Studies, Information Systems'],
+      'dates': '2012-2017',
+      
+    }
+  ],
+  onlineCourses: [ {
+     'title': 'Front-end Web Developer Nanodegree',
+     'school': 'Udacity',
+     'dates': '2017',
+     'url': 'www.udacity.com'
+   
+   }
+  ],
+  display: function() {
+    if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+      for (var i = 0, iLen = education.schools.length; i < iLen; i++) {
+        var formattedName = HTMLschoolName.replace(DATA, education.schools[i].name);
+        var formattedDegree = HTMLschoolDegree.replace(DATA, education.schools[i].degree);
+        var formattedSchoolDates = HTMLschoolDates.replace(DATA, education.schools[i].dates);
+        var formattedLocation = HTMLschoolLocation.replace(DATA, education.schools[i].location);
+        var formattedNameDegree = formattedName + formattedDegree;
+
+        $('#education').append(HTMLschoolStart);
+        $('.education-entry:last').append(formattedNameDegree);
+        $('.education-entry:last').append(formattedSchoolDates);
+        $('.education-entry:last').append(formattedLocation);
+
+        for (var j = 0, jLen = education.schools[i].majors.length; j < jLen; j++) {
+          var formattedMajor = HTMLschoolMajor.replace(DATA, education.schools[i].majors[j]);
+
+          $('.education-entry:last').append(formattedMajor);
+        }
+      }
+
+      $('#education').append(HTMLonlineClasses);
+
+      for (var k = 0, len = education.onlineCourses.length; k < len; k++) {
+        var formattedTitle = HTMLonlineTitle.replace(DATA, education.onlineCourses[k].title).replace('#', education.onlineCourses[k].url);
+        var formattedSchool = HTMLonlineSchool.replace(DATA, education.onlineCourses[k].school);
+        var formattedOnlineDates = HTMLonlineDates.replace(DATA, education.onlineCourses[k].dates);
+        var formattedUrl = HTMLonlineURL.replace(DATA, education.onlineCourses[k].url);
+        var formattedTitleSchool = formattedTitle + formattedSchool;
+
+        $('#education').append(HTMLschoolStart);
+        $('.education-entry:last').append(formattedTitleSchool);
+        $('.education-entry:last').append(formattedOnlineDates);
+        $('.education-entry:last').append(formattedUrl);
+      }
+    }
+  }
+};
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+$('#mapDiv').append(googleMap);
